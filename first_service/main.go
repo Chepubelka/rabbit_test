9f07a80e959ebb4432bdd3228305b516a2236db0
ikasimov <ikasimov@zuzex.com>
2021-02-10 17:49:30 +0300

    Added websocket

--- first_service/main.go
+++ first_service/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"strconv"
-	"encoding/json"
 	"path"
-	"html/template"
+	"strconv"
+
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 	"github.com/streadway/amqp"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
 	handleRequests()
@@ -17,34 +24,69 @@ func main() {
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/counter/{count}", counter)
-	myRouter.HandleFunc("/render/{count}", render)
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
+	myRouter.HandleFunc("/counter", counter)
+	myRouter.HandleFunc("/render", render)
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func render(w http.ResponseWriter, r *http.Request) {
 	fp := path.Join("views", "index.html")
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	if err := tmpl.Execute(w, ""); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func counter(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	vars := mux.Vars(r)
-	count := vars["count"]
-	countInt, _ := strconv.Atoi(count)
-	result := sender(countInt)
-	response, _ := json.Marshal(result)
-	w.Write(response)
+	connect, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+	for {
+		// Read message from browser
+		msgType, msg, err := connect.ReadMessage()
+		if err != nil {
+			return
+		}
+		countInt, _ := strconv.Atoi(string(msg))
+		sender(countInt)
+		conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+		failOnError(err, "Failed to connect to RabbitMQ")
+		defer conn.Close()
+		ch, err := conn.Channel()
+		failOnError(err, "Failed to open a channel")
+		defer ch.Close()
+		new_q, err := ch.QueueDeclare(
+			"test1", // name
+			false,   // durable
+			false,   // delete when unused
+			false,   // exclusive
+			false,   // no-wait
+			nil,     // arguments
+		)
+		failOnError(err, "Failed to declare a queue")
+		num, err := ch.Consume(
+			new_q.Name, // queue
+			"",         // consumer
+			true,       // auto-ack
+			false,      // exclusive
+			false,      // no-local
+			false,      // no-wait
+			nil,        // args
+		)
+		failOnError(err, "Failed to register a consumer")
+		for i := range num {
+			fmt.Printf("%s sent: %s\n", connect.RemoteAddr(), i.Body)
+			// Write message back to browser
+			if err = connect.WriteMessage(msgType, i.Body); err != nil {
+				return
+			}
+		}
+	}
 }
 
-func sender(countInt int) []string{
+func sender(countInt int){
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -72,36 +114,6 @@ func sender(countInt int) []string{
 			})
 		failOnError(err, "Failed to publish a message")
 	}
-
-	new_q, err := ch.QueueDeclare(
-		"test1", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-		)
-		failOnError(err, "Failed to declare a queue")
-		num, err := ch.Consume(
-			new_q.Name, // queue
-			"",     // consumer
-			true,   // auto-ack
-			false,  // exclusive
-			false,  // no-local
-			false,  // no-wait
-			nil,    // args
-		  )
-		  failOnError(err, "Failed to register a consumer")
-		  var responseArray []string
-		  func() {
-			for i := range num {
-				responseArray = append(responseArray, string(i.Body))
-				if (len(responseArray) == countInt) {
-					break
-				}
-			}
-		  }()
-		return responseArray
 }
 
 func failOnError(err error, msg string) {
